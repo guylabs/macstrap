4850945690e2cfdd37631522aeb1ff538fc4a4be
Guy Brand <guy.brand@edorasware.com>
2015-08-12 23:32:55 +0200

    (chore): remove Ubuntu OS and several other redundant files

--- install.sh
+++ install.sh
@@ -20,8 +20,3 @@ fi
 
 # symlink dots
 ln -s "$lib/dots/dots.sh" "$bin/dots"
-
-# Ubuntu-only: Change from dash to bash
-if [ "$BASH_VERSION" = '' ]; then
-  sudo echo "dash    dash/sh boolean false" | debconf-set-selections ; dpkg-reconfigure --frontend=noninteractive dash
-fi
